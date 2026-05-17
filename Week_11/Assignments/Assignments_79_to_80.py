d64b4ce3074aa74187c835013ea4ad5136d7535c
Hamza Zitouni <hamzazitouni250@gmail.com>
2026-05-17 04:55:15 +0100

    Complete Assignments_79_to_80

--- Week_11/Assignments/Assignments_79_to_80.py
+++ Week_11/Assignments/Assignments_79_to_80.py
@@ -6,3 +6,21 @@
 # === First Assignment ===
 # ========================
 
+import datetime
+
+date = datetime.datetime(2021, 6, 25)
+dateNow = datetime.datetime.now()
+
+"Days From 2021-06-25 To 2021-08-10 Is => 46"
+print(f"Days from {date.date()} to {dateNow.date()} is => {(dateNow - date).days}")
+
+# ========================
+# === Second Assignment ===
+# ========================
+
+print(f"Today is {dateNow.date()}")
+print(f"Today is {dateNow.date().strftime("%b %d, %Y")}")
+print(f"Today is {dateNow.date().strftime("%d - %b - %Y")}")
+print(f"Today is {dateNow.date().strftime("%d / %b / %y")}")
+print(f"Today is {dateNow.date().strftime("%d / %B / %Y")}")
+print(f"Today is {dateNow.date().strftime("%a, %d %B %Y")}")
